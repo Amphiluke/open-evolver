066fdfe1313b613d6d4880675ef665f148c84b1f
Amphiluke <amphiluke@gmail.com>
2014-12-02 20:49:54 +0600

    Menu mechanics updated

--- src/js/interface.js
+++ src/js/interface.js
@@ -9,57 +9,113 @@ var $ = global.jQuery,
     $doc = $(document);
 
 
-//(function () {
-//    var ids = [];
-//    ui.lock = function (id) {
-//        if (!ids.length) {
-//            // do lock
-//        }
-//        ids.push(id);
-//    };
-//    ui.unlock = function (id) {
-//        var index = ids.indexOf(id);
-//        if (index > -1) {
-//            ids.splice(index, 1);
-//            if (!ids.length) {
-//                // do unlock
-//            }
-//        }
-//    };
-//})();
-
-ui.potentialTpl = _.template($("#oe-potentials-tpl").html());
-
-ui.setupPotentialGrid = function () {
-    var atoms = OE.structure.atoms,
-        atomList = [],
-        pairs = [],
-        i, j, len;
-    for (i = 0, len = atoms.length; i < len; i++) {
-        if (atomList.indexOf(atoms[i].el) === -1) {
-            atomList.push(atoms[i].el);
-        }
-    }
-    for (i = 0, len = atomList.length; i < len; i++) {
-        for (j = i; j < len; j++) {
-            pairs.push(atomList[i] + atomList[j]);
-        }
-    }
-    $("ul.oe-potentials").html(ui.potentialTpl({pairs: pairs}));
+ui.$ = function (target) {
+    return (target && target.jquery) ? target : $(target);
 };
 
-
-$doc.on("click.oe", function (e) {
-    var target = $(e.target),
-        popups = $("menu.oe-menu menu.expanded");
-    if (target.is("menu.oe-menu button[menu]")) {
-        popups = popups.not($("#" + target.attr("menu")).toggleClass("expanded"));
+ui.proto = {
+    init: function () {
+        var events = this.events || (this.events = {});
+        _.each(events, function (config) {
+            var handler = (typeof config.handler === "string") ? this[config.handler] : config.handler;
+            ui.$(config.owner).on(config.type, config.filter || null, handler.bind(this));
+        }, this);
+        return this;
     }
-    popups.removeClass("expanded");
-});
+};
 
-$("#oe-file").on("change", function () {
-    OE.fileAPI.loadHIN(this.files[0], ui.setupPotentialGrid);
-});
+ui.potentials = (_.extend(Object.create(ui.proto), {
+    $el: $(".oe-potential-form"),
+
+    tpl: _.template($("#oe-potentials-tpl").html()),
+
+    events: [
+        {type: "click", owner: ".oe-discard-potentials", handler: "handleDiscard"},
+        {type: "keyup", owner: $doc, handler: "handleGlobalKeyUp"}
+    ],
+
+    handleDiscard: function () {
+        this.hide();
+    },
+
+    handleGlobalKeyUp: function (e) {
+        if (e.which === 27) {
+            this.hide();
+        }
+    },
+
+    show: function () {
+        this.$el.removeClass("hidden");
+    },
+
+    hide: function () {
+        this.$el.addClass("hidden");
+    },
+
+    setup: function () {
+        var atoms = OE.structure.atoms,
+            atomList = [],
+            pairs = [],
+            i, j, len;
+        for (i = 0, len = atoms.length; i < len; i++) {
+            if (atomList.indexOf(atoms[i].el) === -1) {
+                atomList.push(atoms[i].el);
+            }
+        }
+        for (i = 0, len = atomList.length; i < len; i++) {
+            for (j = i; j < len; j++) {
+                pairs.push(atomList[i] + atomList[j]);
+            }
+        }
+        $("ul.oe-potentials").html(ui.potentials.tpl({pairs: pairs}));
+    }
+})).init();
+
+
+ui.menu = (_.extend(Object.create(ui.proto), {
+    $el: $(".oe-menu"),
+
+    events: [
+        {type: "click.oe", owner: $doc, handler: "handleGlobalClick"},
+        {type: "click", owner: ".oe-menu", filter: "menuitem[data-action]", handler: "handleAction"},
+        {type: "change", owner: "#oe-file", handler: "handleFile"}
+    ],
+
+    handleGlobalClick: function (e) {
+        var target = $(e.target),
+            popups = this.$el.find("menu.expanded");
+        if (target.is(".oe-menu button[menu]")) {
+            popups = popups.not($("#" + target.attr("menu")).toggleClass("expanded"));
+        }
+        popups.removeClass("expanded");
+    },
+
+    handleAction: function (e) {
+        var actionHandler = $(e.target).data("action") + "Action";
+        if (typeof this[actionHandler] === "function") {
+            this[actionHandler]();
+        }
+    },
+
+    handleFile: function (e) {
+        OE.fileAPI.loadHIN(e.target.files[0], ui.potentials.setup);
+    },
+
+    loadAction: function () {
+        $("#oe-file").trigger("click");
+    },
+
+    saveAction: function () {
+        // TODO: implement action
+    },
+
+    setupAction: function () {
+        ui.potentials.show();
+    },
+
+    alterAction: function () {
+        // TODO: implement action
+    }
+})).init();
 
 })(this);
